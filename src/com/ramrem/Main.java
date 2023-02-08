39dd3f59c5c45b2638da1466aaee2b5a8b34f025
SubarMaxKun <maxgamer0027@gmail.com>
2023-02-08 23:47:09 +0200

    Added database, data to database and database connection test code

--- src/com/ramrem/Main.java
+++ src/com/ramrem/Main.java
@@ -1,7 +1,20 @@
 package com.ramrem;
 
-public class Main {
-  public static void main(String[] args){
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class Main {
+
+  public static void main(String[] args) throws SQLException {
+    try (Connection connection = getNewConnection()) {
+      assert (connection.isValid(1));
+      assert (connection.isClosed());
+    }
+  }
+
+  private static Connection getNewConnection() throws SQLException {
+    String url = "jdbc:sqlite:C:/Users/subar/Desktop/OOP_PW7/src/com/ramrem/databases/sto.db";
+    return DriverManager.getConnection(url);
   }
 }
